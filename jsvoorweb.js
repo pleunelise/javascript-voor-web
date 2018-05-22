29c49da95ffc2a5c83237b696c428499cb31fa47
Pleun Emmelot <pleunelise.emmelot@coderclass.nl>
2018-05-22 09:50:50 +0200

    opdracht 2.2

--- jsvoorweb.js
+++ jsvoorweb.js
@@ -1,6 +1,4 @@
-var woord1 = "hello";
-var woord2 = "world";
-var x = Math.floor((Math.random() * 20) + 1);
-for (var i = 0; i < x; i++ ) {
-  alert(woord1 + " " + woord2);
-}
+var mijnPara = document.getElementById("para1");
+
+alert("Hier is de innerHTML: " + mijnPara.innerHTML);
+alert("Hier is de  outerHTML: " + mijnPara.outerHTML);
